8b7df85b787ddd49dbb48fa3c3ded925cb0bd589
vishal <mr.vishalpunjabi@gmail.com>
2022-01-24 20:25:29 -0800

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask
-  
+import os
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
@@ -20,4 +20,4 @@ if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
